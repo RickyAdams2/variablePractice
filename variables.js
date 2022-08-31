4a96b1febaa53b08ae3f357154d3838c401a9de3
Ricky Adams <mdaylon98@gmail.com>
2022-08-31 10:53:01 -0400

    Added variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+var = firstName = "Eugene"
+var = petName = "Gary"
+var = dateOfBirth = "January 3rd"
+
+var = streetNumber = 1333
+var = petAge = 35
+var = bloodCount = 1500
+
+var = isMale = true
+var = hasPet = true
+var = isOld= false
