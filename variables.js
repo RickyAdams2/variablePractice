d673d1b56e5ddb198acd803a5068c5b1f8b53245
Ricky Adams <mdaylon98@gmail.com>
2022-08-31 11:12:33 -0400

    Node

--- variables.js
+++ variables.js
@@ -1,12 +1,35 @@
-var = firstName = "Eugene"
-var = petName = "Gary"
-var = dateOfBirth = "January 3rd"
+// var = firstName = "Eugene"
+var firstName = "Eugene"
+// var = petName = "Gary"
+var petName = "Gary"
+// var = dateOfBirth = "January 3rd"
+var dateOfBirth = "January 3rd"
 
-var = streetNumber = 1333
-var = petAge = 35
-var = bloodCount = 1500
+// var = streetNumber = 1333
+var streetNumber = 1333
+// var = petAge = 35
+var petAge = 35
+// var = bloodCount = 1500
+var bloodCount = 1500
 
-var = isMale = true
-var = hasPet = true
-var = isOld= false
-var sentence = `$``{firstName} his name is`; console.log(sentence);
+// var = isMale = true
+var isMale = true
+// var = hasPet = true
+var hasPet = true
+// var = isOld= false
+var isOld = false
+// var sentence = `$``{firstName} his name is`; console.log(sentence);
+var sentence = `${petName} name of pet`; console.log(sentence);
+firstName = "Barry"
+petName = "Fred"
+dateOfBirth = "July 5th"
+
+console.log(firstName);
+console.log(petName);
+console.log(dateOfBirth);
+console.log(streetNumber);
+console.log(petAge);
+console.log(bloodCount);
+console.log(isMale);
+console.log(hasPet);
+console.log(isOld);
